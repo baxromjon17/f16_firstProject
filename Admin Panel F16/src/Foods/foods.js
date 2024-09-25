9162870699b31ab6a7e244f5d8a55fc03c16df2b
baxromjon17 <gofurov.baxromjon17@gmail.com>
2024-09-25 20:34:35 +0300

    Update tayyor

--- Admin Panel F16/src/Foods/foods.js
+++ Admin Panel F16/src/Foods/foods.js
@@ -36,3 +36,50 @@ form.addEventListener("submit", (e) =>{
   }
 
 })
+
+
+
+ async function createFoodTodo(){
+  let response = await fetch("https://66e5ad0f5cc7f9b6273e0ca9.mockapi.io/f16")
+
+  .then(res => res.json())
+  .then(data => {
+    let todoRoot = document.querySelector(".main_container");
+
+      data.map((item)=>{
+        todoRoot.innerHTML +=`
+        
+        <div class="to_do">
+        <h1>${item.id}</h1>
+        <h1>${item.name}</h1>
+        <h3>${item.price}$</h3>
+        <img src="${item.img}">
+        <button style="border-radius: 15px; background-color: red; color: white; border: none; padding: 8px 13px;">Delete</button>
+        <button style="border-radius: 15px; background-color: yellow; border: none; padding: 8px 13px;" onclick="updateTodo(${item.id})">Update</button>
+    </div>
+`
+      })
+
+  })
+  
+  console.log(data);
+
+}
+
+createFoodTodo()
+
+function updateTodo(id){
+  console.log(id);
+
+  let update = document.querySelector(".update")
+  update.classList.add("active")
+}
+
+
+
+
+
+function backMenu(){
+  let update = document.querySelector(".update")
+  update.classList.remove("active")
+}
